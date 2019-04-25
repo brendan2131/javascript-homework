22d46f05c135a98af55efc22c6c0cc015931db39
Brendan <brendan26@sbcglobal.net>
2019-04-25 12:13:42 -0500

    added code

---
+++ static/js/app.js
@@ -0,0 +1,40 @@
+// from data.js
+var tableData = data;
+
+
+
+// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+function renderTable (renderData) {
+  renderData.forEach((ufoSighting) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(([key, value]) => {
+      var cell = tbody.append("td");
+      cell.text(value);
+    });
+});
+};
+
+renderTable(tableData);
+
+var submit = d3.select("#filter-btn");
+
+submit.on("click", function() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var inputValue = d3.select("#datetime").node().value;
+
+
+  console.log(inputValue);
+  console.log(tableData);
+
+  var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+
+  console.log(filteredData);
+
+  renderTable(filteredData);
+});
